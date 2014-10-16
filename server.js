f729317d942ea4152921cfabc2e280aba65d903d
Loveemtrees <sebas.baum@gmail.com>
2014-10-16 21:40:27 +0200

    refactored the mapping, made full use of express and socket.io shortcuts

--- server.js
+++ server.js
@@ -1,75 +1,34 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var server;
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-server = http.createServer(function(req, res){
-    // your normal server code
-
-    //MAPPING
-    var path = url.parse(req.url).pathname;
-    switch (path){
-        case '/':
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write('<h1>Hello! Try the <a href="/public/student.html">Student page</a></h1><br>' +
-                      '<h1>OR Try the <a href="/public/host.html">Host page</a></h1> ');
-            res.end();
-            break;
-        case '/public/student.html':
-            fs.readFile(__dirname + path, function(err, data){
-                if (err){ 
-                    return send404(res);
-                }
-                res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'});
-                res.write(data, 'utf8');
-                res.end();
-            });
-        case '/public/host.html':
-            fs.readFile(__dirname + path, function(err, data){
-                if (err){
-                    return send404(res);
-                }
-                res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'});
-                res.write(data, 'utf8');
-                res.end();
-            });
-        case '/public/style.css':
-            fs.readFile(__dirname + path, function(err, data){
-                if (err) return send404(res);
-                res.writeHead(200, { 'Content-Type': 'text/css' });
-                res.end(data, 'utf-8');
-                res.end();
-            });
-        break;
-        default: send404(res);
-    }
+// Mapping (Route Handlers)
+app.get('/', function(req, res){
+    res.send("<h1>Hello! Try the <a href='/student'>Student page</a></h1><br>" +
+    "<h1>OR Try the <a href='/host'>Host page</a></h1>");
+});
+app.get('/student', function(req, res){
+    res.sendFile(__dirname + '/public/student.html');
+});
+app.get('/host', function(req, res){
+    res.sendFile(__dirname + '/public/host.html');
+});
+app.get('/style.css', function(req, res){
+    res.sendFile(__dirname + '/public/style.css');
 });
 
-send404 = function(res){
-    res.writeHead(404);
-    res.write('404');
-    res.end();
-};
-
-server.listen(8001);
-
-// use socket.io
-var io = require('socket.io').listen(server);
-
-//turn off debug
-io.set('log level', 1);
+// define port
+http.listen(8001, function(){
+    console.log('listening on Port :8001');
+});
 
 // define interactions with client
-io.sockets.on('connection', function(socket){
+io.on('connection', function(socket){
     //send data to client
+    console.log("a user connected");
     setInterval(function(){
         socket.emit('date', {'date': new Date()});
     }, 1000);
-
-    //receive client data - NOT USED
-    socket.on('client_data', function(data){
-        process.stdout.write(data.letter);
-    });
 
     //receive host data
     socket.on('host_message', function(msg){
@@ -80,4 +39,5 @@ io.sockets.on('connection', function(socket){
     socket.on('client_message', function(msg){
         io.emit("client_message", msg);
     });
+
 });
