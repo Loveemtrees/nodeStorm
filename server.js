6e4c509442f8d10359161068b49dc19e4109c567
Loveemtrees <sebas.baum@gmail.com>
2014-10-20 18:19:33 +0200

    implemented connected user count, added scroll box for many replies

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var connectCounter = -1;
+
 // Mapping (Route Handlers)
 app.get('/', function(req, res){
     res.send("<h1>Hello! Try the <a href='/student'>Student page</a></h1><br>" +
@@ -23,21 +25,37 @@ http.listen(8001, function(){
 });
 
 // define interactions with client
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
     //send data to client
+
+    // log and count
     console.log("a user connected");
-    setInterval(function(){
+    connectCounter++;
+    console.log(connectCounter);
+    io.emit("counter_update", connectCounter);
+
+    // messages
+    // date
+    setInterval(function () {
         socket.emit('date', {'date': new Date()});
     }, 1000);
 
-    //receive host data
-    socket.on('host_message', function(msg){
+    //receive and emit host data
+    socket.on('host_message', function (msg) {
         io.emit("host_message", msg);
     });
 
-    //receive client data
-    socket.on('client_message', function(msg){
+    //receive and emit client data
+    socket.on('client_message', function (msg) {
         io.emit("client_message", msg);
     });
 
+    socket.on('disconnect', function(){
+        // log and count
+        console.log("a user disconnected");
+        connectCounter--;
+        console.log(connectCounter);
+        io.emit("counter_update", connectCounter);
+    });
 });
+
