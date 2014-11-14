94364f86008b93e7cbe945acc549cd4f7b682d41
Loveemtrees <sebas.baum@gmail.com>
2014-11-14 17:29:04 +0100

    history working, replyCounter working, connect after Question working

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ var HashMap = require('hashmap').HashMap;
 var connectCounter = -1;
 
 var lastQuestion = "";
+var currentQuestion = "";
 var map = new HashMap();
 var replies = [];
+var replyCounter = 0;
 
 // Mapping (Route Handlers)
 app.get('/', function(req, res){
@@ -47,8 +49,11 @@ io.on('connection', function(socket) {
 
     //receive and emit host data
     socket.on('host_message', function (msg) {
+        currentQuestion = msg;
         io.emit("host_message", msg);
-        // save question and replies in hashmap
+        // receive replies for new question set to 0
+        replyCounter = 0;
+        // save question and replies in hashmap when new question sent
         if (lastQuestion != "") {
             map.set(lastQuestion, replies);
             lastQuestion = msg;
@@ -67,6 +72,13 @@ io.on('connection', function(socket) {
         replies.push(msg);
     });
 
+    // receive true msg on first reply from client
+    socket.on("reply_status", function (msg){
+        msg ? replyCounter++ : "";
+        //map.get(currentQuestion).length;
+        io.emit("replyCounter", replyCounter);
+    });
+
     socket.on('disconnect', function(){
         // log and count
         console.log("a user disconnected");
@@ -79,6 +91,16 @@ io.on('connection', function(socket) {
     socket.on('get_replies', function(msg){
         console.log(map.get(msg));
         io.emit('requested_replies', map.get(msg));
+    });
+
+    // for the active question we reply with the current replies array
+    socket.on('get_replies_for_actual', function(){
+        io.emit('requested_replies', replies);
+    });
+
+    // send current question to client for when he connected after question has been sent
+    socket.on('check_for_questions', function(){
+        io.emit('current_question', currentQuestion);
     });
 });
 
