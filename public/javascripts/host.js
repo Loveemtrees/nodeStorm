7581f9b3f0a95ab93d433d2b5d43dbf627aa4078
Loveemtrees <sebas.baum@gmail.com>
2015-01-26 14:52:05 +0100

    adding comments

--- public/javascripts/host.js
+++ public/javascripts/host.js
@@ -4,13 +4,13 @@ var replies = [];
 $(document).ready(function(){
 
     // STATUS ---------------------------------------------
-    socket.on('connect_error', function(err) {
+    socket.on('connect_error', function (err) { // disconnect
         $("#connected").hide();
         $("#disconnected").show();
         $("#connection").removeClass("connection-ok");
         $("#connection").addClass("connection-broken");
     });
-    socket.on('connect', function() {
+    socket.on('connect', function () { // connected
         $("#disconnected").hide();
         $("#connected").show();
         $("#connection").removeClass("connection-broken");
@@ -19,30 +19,32 @@ $(document).ready(function(){
 
     // MESSAGE TO CLIENTS------------------------------------>
     $("form").submit(function () {
-        socket.emit("host_message", $("#m").val());
-        $("#replies > li").remove();
-        $("#messages > .list-group-item").removeClass("active");
-        $("#messages").prepend($("<li>").addClass("list-group-item active").text($("#m").val()));
-        $("#reply_count").text("0");
-        replies = [];
-        $("#m").val("");
+        socket.emit("host_message", $("#m").val());                         // send out the question
+        $("#replies > li").remove();                                        // remove replies from DOM
+        $("#messages > .list-group-item").removeClass("active");            // unmark last question as active
+        $("#messages").prepend($("<li>").addClass("list-group-item active")
+            .text($("#m").val()));                                          // insert new question to the list
+        $("#reply_count").text("0");                                        // reset reply count display
+        replies = [];                                                       // reset local replies array
+        $("#m").val("");                                                    // empty input field
         return false;
     });
     socket.on("client_message", function (msg) {
-        if($("#messages .list-group-item:first-of-type").is(".active")) {
-            $("#replies").prepend($("<li>").addClass("list-group-item").text(msg));
+        if ($("#messages .list-group-item:first-of-type").is(".active")) {           // check if current question is selected
+            $("#replies").prepend($("<li>").addClass("list-group-item").text(msg)); // insert replies into DOM
         }
-        replies ++;
-        $("#messages .list-group-item:first-of-type span").remove();
-        $("#messages .list-group-item:first-of-type").append($("<span>").addClass("badge").text(replies));
+        replies++;                                                                 // increment local replies array
+        $("#messages .list-group-item:first-of-type span").remove();                // remove old badge
+        $("#messages .list-group-item:first-of-type").append($("<span>")
+            .addClass("badge").text(replies));                                      // insert badge with reply count
     });
     // receive the requested replies and implement into DOM-tree
     socket.on("requested_replies", function (msg) {
-        console.log(msg);
-        $("#replies li").remove();
-        for (var i = 0; i < msg.length; i++){
-            var reply = msg[i];
-            $("#replies").prepend($("<li>").fadeIn("slow").addClass("list-group-item").text(reply));
+        $("#replies li").remove();                          // remove replies from DOM
+        for (var i = 0; i < msg.length; i++) {               // run through replies
+            var reply = msg[i];                             // current reply
+            $("#replies").prepend($("<li>").fadeIn("slow")
+                .addClass("list-group-item").text(reply));  // let reply appear in DOM
         }
     });
     // Users replied to current question
@@ -59,13 +61,13 @@ $(document).ready(function(){
     // --------------------------------------------------------
 
     // click on a message list element will send a request for related replies to server
-    $("#messages").on("click", ".list-group-item", function(){
-        if( !$(this).is(":first-of-type") ){
-            $("#messages > .list-group-item").removeClass("active");
-            $(this).addClass("active");
-            // Clean text from number of badge
-            var textWithoutNumber = $(this).text().replace(/\d+$/, '');
-            socket.emit("get_replies", textWithoutNumber);
+    $("#messages").on("click", ".list-group-item", function () {          // click on a question
+        if (!$(this).is(":first-of-type")) {                            // if old question
+            $("#messages > .list-group-item").removeClass("active");    // unmark current question as active
+            $(this).addClass("active");                                 // mark clicked question active
+            // clean text from number of badge
+            var textWithoutNumber = $(this).text().replace(/\d+$/, ''); // regex to remove numbers
+            socket.emit("get_replies", textWithoutNumber);              // send cleaned string to server
         }
     });
     // make clicked item active and send content to server
